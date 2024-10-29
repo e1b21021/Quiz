bea9417ebcd8b508e164fdda27436410704d04cd
e1b21021 <e1b21021@oit.ac.jp>
2024-10-29 15:40:23 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/quizrock/quiz6/Quiz6Application.java
@@ -0,0 +1,13 @@
+package oit.is.quizrock.quiz6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Quiz6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Quiz6Application.class, args);
+	}
+
+}
